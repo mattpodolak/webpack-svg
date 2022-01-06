7297b476ca0f2d25b99be41bbd655a344796bda6
Matthew Podolak <mpodola2@gmail.com>
2022-01-06 10:49:20 -0500

    recreate svg issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +21,43 @@ function App() {
     </div>
   );
 }
+
+export const Logo = () => {
+  function useDynamicSVGImport(name) {
+    const ImportedIconRef = useRef();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+      setLoading(true);
+      const importIcon = async () => {
+        try {
+          ImportedIconRef.current = (
+            await import(`./${name}.svg`)
+          ).ReactComponent;
+        } catch (err) {
+          setError(err);
+          throw err;
+        } finally {
+          setLoading(false);
+        }
+      };
+      importIcon();
+    }, [name]);
+
+    return { error, loading, SvgIcon: ImportedIconRef.current };
+  }
+
+  const { loading, SvgIcon } = useDynamicSVGImport('logo');
+
+  if (loading) {
+    return null;
+  }
+
+  if (SvgIcon) {
+    return <SvgIcon />;
+  }
+  return null;
+};
 
 export default App;
